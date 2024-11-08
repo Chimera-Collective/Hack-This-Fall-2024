1dfabacc785b7f34f04a1e2023388254c56ef883
Silver Alcid <silveralcid@gmail.com>
2024-11-08 18:56:38 -0500

    test: Demo SSL implemented, camera capture working with desktop webcam and iOS iPhone 13.

--- client-app/vite.config.ts
+++ client-app/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), mkcert()],
+  server: {
+    https: true,
+    host: true, // Expose to all network interfaces
+    port: 5173, // Optional: specify port explicitly
+  },
 })
